46755a749d2a3c5547e63d6371fdb0d0d972c55f
Hieu Nguyen <hieunguyen6524@gmail.com>
2025-07-31 18:52:58 +0700

    Improved stripe messages

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,7 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.alerts = (req, res, next) => {
-  const { alerts } = req.require;
+  const { alerts } = req.query;
   if (alerts === 'booking') {
     res.locals.alerts =
       "Your booking was successfil! Please check your email for a confirmation. If your booking doesn't show up here immediatly, please come back later.";
