f623a349703d18228099959ab229c80a65679dd0
Hieu Nguyen <hieunguyen6524@gmail.com>
2025-07-31 18:21:50 +0700

    Improved stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -65,7 +65,7 @@ exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    event = stripe.webhooks.construcEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET,
